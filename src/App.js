bc43300d85f298907dd8b6895622edfab0a102f0
Jasmine Francois <francois.jasminem@gmail.com>
2023-03-27 20:53:30 -0500

    Add input with change logging

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import './App.css';
 
 function App() {
-  let todoListItems = [
-      'Laundry',
-      'Dishes',
-      'Sweep the floor',
-      'Feed my cats',
-      'Clean the shower',
-      'Random task A',
-      'Another task'
-  ];
+    let todoListItems = [
+        'Laundry',
+        'Dishes',
+        'Sweep the floor',
+        'Feed my cats',
+        'Clean the shower',
+        'Random task A',
+        'Another task',
+        'Another task',
+    ];
 
-  return (
-    <div>
-      <ul>
-        {
-          // I can do JS here
-            todoListItems.map((item) => {
-              return (<li>{item}</li>)
-            })
-        }
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                {
+                    // I can do JS here
+                    todoListItems.map((item, index) => {
+                        return (<li key={index}>{item}</li>)
+                    })
+                }
+            </ul>
+            {/* events must be camel cased */}
+            <input
+                onChange={(event) => {
+                    console.log(event.target.value)
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
