15360a2a5752c8428e843885a20969cafca56e67
Jasmine Francois <francois.jasminem@gmail.com>
2023-04-03 19:18:10 -0500

    Saving state and using onChange event to trigger state update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import './App.css';
+import React from 'react';
 
 // two ways to declare a component, both will work
 // const App = () => { // inside of curly braces will remain the same }
 
 function App() {
-    let todoListItems = [
-        'Laundry',
-        'Dishes',
-        'Sweep the floor',
-        'Feed my cats',
-        'Clean the shower',
-        'Random task A',
-        'Another task',
-        'Another task',
-    ];
+    const [listItems, setListItems] = React.useState([]);
+    // this is basically equivalent to this....
+    /*
+    const useState = () => {
+        // logic goes here
 
+        return [yourVariable, setYourVariable]
+    }
+     */
     return (
         <div>
             <ul>
                 {
                     // I can do JS here
-                    todoListItems.map((item, index) => {
+                    listItems.map((item, index) => {
                         return (<li key={index}>{item}</li>)
                     })
                 }
             </ul>
             {/* events must be camel cased */}
             <input
-                onMouseOver={(event) => {
-                    console.log('moused over!')
-                }}
                 onChange={(event) => {
+                    setListItems([event.target.value])
                     console.log(event.target.value)
                 }}
             />
