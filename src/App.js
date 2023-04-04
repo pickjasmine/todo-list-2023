8ce6d8fb1e5a9fe05897be5fc12a529a8133a394
Jasmine Francois <francois.jasminem@gmail.com>
2023-04-03 19:36:33 -0500

    Saving user input when they press the add button

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import './App.css';
 import React from 'react';
 
-// two ways to declare a component, both will work
-// const App = () => { // inside of curly braces will remain the same }
-
 function App() {
-    const [listItems, setListItems] = React.useState([]);
-    // this is basically equivalent to this....
-    /*
-    const useState = () => {
-        // logic goes here
 
-        return [yourVariable, setYourVariable]
-    }
-     */
+    const [userEnteredTodo, setUserEnteredTodo] = React.useState();
+    const [listItems, setListItems] = React.useState([]);
+
+    console.log(userEnteredTodo)
+
     return (
         <div>
             <ul>
                 {
-                    // I can do JS here
                     listItems.map((item, index) => {
                         return (<li key={index}>{item}</li>)
                     })
                 }
             </ul>
-            {/* events must be camel cased */}
             <input
                 onChange={(event) => {
-                    setListItems([event.target.value])
+                    setUserEnteredTodo(event.target.value)
                     console.log(event.target.value)
                 }}
             />
+            <button
+                onClick={() => {
+                    console.log('here')
+                    setListItems([
+                        ...listItems,
+                        userEnteredTodo
+                    ])
+                }}
+            >Add</button>
         </div>
     );
 }
